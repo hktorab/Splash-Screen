519e18f18521df22e3456254b67dfb84930d1c4f
Humayun Kabir <humayunkabirtorab@gmail.com>
2016-12-04 04:11:32 +0600

    Initial commit

---
+++ app/src/main/java/com/example/torab/splashscreen/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.example.torab.splashscreen;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    Thread myThread = new Thread(){
+        @Override
+        public void run() {
+            try {
+                sleep(3000);
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+        myThread.start();
+    }
+}
